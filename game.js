26af7bf9e4b26f3d479bee925cc287654565c247
Soham <chitradana81@gmail.com>
2022-05-30 21:15:47 +0530

    setup text game

---
+++ game.js
@@ -0,0 +1,32 @@
+const textElement = document.getElementById("text");
+const optionButtons = document.getElementById("option-buttons");
+
+let state = {};
+
+function startGame() {
+  state = {};
+  showTextNode(1);
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
+  textElement.innerText = textNode.text;
+}
+
+function selectOption(option) {}
+
+const textNodes = [
+  {
+    id: 1,
+    text: "You see a sword beside you...",
+    options: [
+      { text: "Take it", setState: { sword: True }, nextText: 2 },
+      { text: "Leave it", nextText: 2 },
+    ],
+  },
+  {
+    id: 2,
+  },
+];
+
+startGame();

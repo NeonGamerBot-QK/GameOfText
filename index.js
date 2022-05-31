057212197e8e0e0829c64e7b8869a97cc56c2f3e
Soham <chitradana81@gmail.com>
2022-05-31 16:55:33 +0530

    made more games :)

---
+++ index.js
@@ -0,0 +1,15 @@
+function adv() {
+  location.href = "/pages/adventure.html";
+}
+
+function fantasy() {
+  location.href = "/pages/fantasy.html";
+}
+
+function scifi() {
+  location.href = "/pages/scifi.html";
+}
+
+function magic() {
+  location.href = "/pages/magic.html";
+}

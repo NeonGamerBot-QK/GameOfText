8d7be788270fee476a9118bbd2c7deff281a75c9
Soham <chitradana81@gmail.com>
2022-05-31 12:31:54 +0530

    setup js functions

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const textElement = document.getElementById("text");
-const optionButtons = document.getElementById("option-buttons");
+const optionButtonsElement = document.getElementById("option-buttons");
 
 let state = {};
 
@@ -11,9 +11,34 @@ function startGame() {
 function showTextNode(textNodeIndex) {
   const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
   textElement.innerHTML = textNode.text;
+
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
+
+  textNode.options.forEach((option) => {
+    if (showOption(option)) {
+      const button = document.createElement("button");
+      button.innerText = option.text;
+      button.classList.add("btn");
+      button.addEventListener("click", () => selectOption(option));
+      optionButtonsElement.appendChild(button);
+    }
+  });
 }
 
-function selectOption(option) {}
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state);
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText;
+  if (nextTextNodeId <= 0) {
+    startGame();
+  }
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNodeId);
+}
 
 const textNodes = [
   {
@@ -26,6 +51,15 @@ const textNodes = [
   },
   {
     id: 2,
+    text: "You open a strange door and find yourself in a world, which is weirdly beautiful.",
+    options: [
+      {
+        text: "You ask people where you are",
+        //requiredState: (currentState) => currentState.sword,
+        nextText: 3,
+      },
+      { text: "You explore the world", nextText: 4 },
+    ],
   },
 ];
 

398b7d7028b2c0e80b8a514fcdd0361684e65fc4
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2022-06-17 00:14:46 -0400

    PR make it look better & tested

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GameOfText
 A website that's also a game, also a text game, the GameOfText!
-
+[link](https://gameoftext.netlify.app)
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/a704e5fd-a316-4dfc-b9bd-0256cc62b1ac/deploy-status)](https://gameoftext.netlify.app)
 
--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-function distanceFromCenter(boxPosition, mousePosition, boxSize) {
-  return boxPosition - mousePosition + boxSize / 2;
-}
-
-function adv() {
-  location.href = "/pages/adventure.html";
-}
-
-function fantasy() {
-  location.href = "/pages/fantasy.html";
-}
-
-function anime() {
-  location.href = "/pages/anime.html";
-}
-
-function scifi() {
-  location.href = "/pages/scifi.html";
-}
-
-function magic() {
-  location.href = "/pages/magic.html";
-}
+(function () {
+  window.distanceFromCenter = function (boxPosition, mousePosition, boxSize) {
+    return boxPosition - mousePosition + boxSize / 2;
+  }
+  
+  // function adv() {
+  //   location.href = "/pages/adventure.html";
+  // }
+  
+  // function fantasy() {
+  //   location.href = "/pages/fantasy.html";
+  // }
+  
+  // function anime() {
+  //   location.href = "/pages/anime.html";
+  // }
+  
+  // function scifi() {
+  //   location.href = "/pages/scifi.html";
+  // }
+  
+  // function magic() {
+  //   location.href = "/pages/magic.html";
+  // }
+  window.addEventListener("load", () => {
+    const data = [
+      { "path": "/pages/adventure.html", "id": "adventure" },
+      { "path": "/pages/fantasy.html", "id": "fantasy" },
+      { "path": "/pages/anime.html", "id": "anime" },
+      { "path": "/pages/scifi.html", "id": "sci-fi" },
+      { "path": "/pages/magic.html", "id": "magic" }
+    ]
+    data.forEach(({ path, id }) => {
+      const el = document.querySelector(`#${id}`);
+      if(el) {
+        el.addEventListener("click", (e) => {
+          e.preventDefault()
+          location.href = `${window.location.protocol}//${window.location.hostname}${path}`
+        })
+      }
+    })
+  })
+})()
